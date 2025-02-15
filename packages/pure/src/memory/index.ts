28855d07c67fec3543153c7a3a5916c7b7a8012e
Michael Zhao <44533763+Pistonight@users.noreply.github.com>
2025-02-14 18:28:50 -0800

    create pure/memory and add external weak ref (#24)

---
+++ packages/pure/src/memory/index.ts
@@ -0,0 +1,8 @@
+/**
+ * # pure/memory
+ * JS memory utilities
+ *
+ * @module
+ */
+export { cell, type CellConstructor, type Cell } from "./cell.ts";
+export { persist, type PersistConstructor, type Persist } from "./persist.ts";
