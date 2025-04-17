7f3f661f71ce7142bddf3dfcc27051f4689e4bea
Michael Zhao <44533763+Pistonight@users.noreply.github.com>
2025-04-17 03:58:32 -0700

    mechanism for waiting for locale to be ready before firing notification (#35)
    * mechanism for waiting for locale to be ready before firing notification
    
    * pnpm dedupe
    
    * don't call hook during init

--- packages/pure-i18next/src/connect.ts
+++ packages/pure-i18next/src/connect.ts
@@ -1,10 +1,6 @@
-import type { i18n, LanguageDetectorModule, ThirdPartyModule } from "i18next";
+import type { LanguageDetectorModule } from "i18next";
 
-import {
-    addLocaleSubscriber,
-    getLocale,
-    setLocale,
-} from "@pistonite/pure/pref";
+import { getLocale, setLocale } from "@pistonite/pure/pref";
 
 /**
  * Language detector plugin for i18next
@@ -14,21 +10,5 @@ export const syncI18nextToPureModule: LanguageDetectorModule = {
     detect: getLocale,
     cacheUserLanguage: (language: string): void => {
         setLocale(language);
-    },
-};
-
-/**
- * Bind the locale state to i18next, so whenever `setLocale`
- * is called, it will also call `i18next.changeLanguage`.
- *
- */
-export const syncPureToI18nextModule: ThirdPartyModule = {
-    type: "3rdParty" as const,
-    init: (i18next: i18n): void => {
-        addLocaleSubscriber((locale) => {
-            if (i18next.language !== locale) {
-                i18next.changeLanguage(locale);
-            }
-        }, true);
     },
 };
