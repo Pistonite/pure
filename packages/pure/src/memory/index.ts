bcac54c9362d93a7d408dec15be4a4ff25ff2245
Pistonight <pistonknight@outlook.com>
2025-07-07 18:05:40 -0700

    forgot to export

--- packages/pure/src/memory/index.ts
+++ packages/pure/src/memory/index.ts
@@ -8,3 +8,4 @@ export { cell, type CellConstructor, type Cell } from "./cell.ts";
 export { persist, type PersistConstructor, type Persist } from "./persist.ts";
 export * from "./erc.ts";
 export * from "./async_erc.ts";
+export * from "./idgen.ts";
