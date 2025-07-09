3b30449c3bb1d3e7e7d17221447cbc801bcd9f39
Pistonight <pistonknight@outlook.com>
2025-07-08 21:10:26 -0700

    clean up

--- packages/pure-i18next/src/index.ts
+++ packages/pure-i18next/src/index.ts
@@ -2,4 +2,4 @@ export * from "./types.ts";
 export * from "./connect.ts";
 export * from "./backend.ts";
 export * from "./init.ts";
-export { logLevel as changeLogger } from "./util.ts";
+export { logLevel } from "./util.ts";
