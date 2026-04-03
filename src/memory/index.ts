cd8430c4fdfbb43b9fd83f613e513dcc8ba7201f
Pistonight <pistonknight@outlook.com>
2026-04-02 21:02:48 -0700

    chore: remove dom from lib reference

--- src/memory/index.ts
+++ src/memory/index.ts
@@ -4,6 +4,6 @@
  * @module
  */
 export { cell, type CellConstructor, type Cell } from "./cell.ts";
-export { persist, type PersistConstructor, type Persist } from "./persist.ts";
+export { persist, type PersistConstructor, type Persist, type PersistStorage } from "./persist.ts";
 export * from "./emp.ts";
 export * from "./idgen.ts";
